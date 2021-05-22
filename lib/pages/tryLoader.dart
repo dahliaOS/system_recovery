6c34f081cfe02595ebf9571b26f86bac06c6071a
Noah Cain <nmcain@gmail.com>
2021-05-21 17:50:15 -0700

    Enable the live session

---
+++ lib/pages/tryLoader.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'recovery.dart';
+
+class TryLoader extends StatefulWidget {
+  @override
+  _TryLoaderState createState() => _TryLoaderState();
+}
+
+class _TryLoaderState extends State<TryLoader> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+          Padding(
+              padding: EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.launch,
+                    size: 32,
+                    color: Colors.deepOrange,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showPowerMenu(context);
+                      },
+                      icon: Icon(Icons.power_settings_new))
+                ],
+              ) //Image.asset('lib/logo-color.png',width: 165.0, height: 32.0, fit: BoxFit.fill)
+              ),
+          Padding(
+              padding: EdgeInsets.only(
+                left: 25,
+              ),
+              child: Text('Try dahliaOS',
+                  style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontWeight: FontWeight.w300,
+                      fontSize: 25,
+                      color: Colors.grey[900]))),
+          Expanded(
+            child: Center(
+                child: Container(
+              height: 350,
+              margin: EdgeInsets.only(left: 25, right: 25),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 15,
+                  ),
+                  Text("Loading the live session..."),
+                  Container(
+                    height: 25,
+                  ),
+                  LinearProgressIndicator(
+                    value: null,
+                    semanticsLabel: 'Linear progress indicator',
+                  ),
+                ],
+              ),
+            )),
+          ),
+          Align(
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                Padding(
+                    padding: EdgeInsets.only(top: 20.0, right: 20, bottom: 15),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Back'))),
+              ]))
+        ]));
+  }
+}
