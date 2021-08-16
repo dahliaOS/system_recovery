f4fe36cc513f7c61622790a40c4fad341af51c40
Noah Cain <nmcain@gmail.com>
2021-08-16 08:08:31 -0700

    Update disk selector

--- lib/pages/diskSelection.dart
+++ lib/pages/diskSelection.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:recovery/buildInfo.dart';
 import 'dart:io';
 import 'recovery.dart';
 import '../widgets/diskWidget.dart';
@@ -161,34 +162,143 @@ class _DiskSelectionState extends State<DiskSelection> {
                             EdgeInsets.only(top: 20.0, right: 20, bottom: 15),
                         child: ElevatedButton(
                             onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  // return object of type Dialog
-                                  return AlertDialog(
-                                    title: Text(
-                                        "Erase disk and install dahliaOS?"),
-                                    content: Text(
-                                        "All data on the disk /dev/sda will be erased. This action cannot be undone. Do you want to continue?"),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text("CANCEL"),
-                                      ),
-                                      // usually buttons at the bottom of the dialog
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text("ERASE DISK"),
-                                      ),
-                                    ],
-                                  );
-                                },
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DiskConfirmation()),
                               );
                             },
+                            child: Text('Install'))),
+                  ],
+                )
+              ]))
+        ]));
+  }
+}
+
+class DiskConfirmation extends StatefulWidget {
+  final String disk;
+  const DiskConfirmation({Key key, this.disk})
+      : super(
+          key: key,
+        );
+
+  @override
+  State<DiskConfirmation> createState() => _DiskConfirmationState();
+}
+
+class _DiskConfirmationState extends State<DiskConfirmation> {
+  bool install = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+          Padding(
+              padding: EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.delete_forever_outlined,
+                    size: 32,
+                    color: Colors.deepOrange,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showPowerMenu(context);
+                      },
+                      tooltip: "Power Options",
+                      icon: Icon(Icons.power_settings_new))
+                ],
+              ) //Image.asset('lib/logo-color.png',width: 165.0, height: 32.0, fit: BoxFit.fill)
+              ),
+          Padding(
+              padding: EdgeInsets.only(left: 25, bottom: 15),
+              child: Text('Erase disk?',
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w300,
+                    fontSize: 25,
+                    color: Theme.of(context).colorScheme.foregroundText,
+                  ))),
+          Expanded(
+            child: Center(
+                child: Container(
+              height: 350,
+              margin: EdgeInsets.only(left: 25, right: 25),
+              child: ListView(
+                children: <Widget>[
+                  formAlert(Colors.red, "The entire disk will be erased.",
+                      Color(0xFFffffff), Icons.dangerous),
+                  Container(
+                    height: 16,
+                  ),
+                  Text(
+                      'The following changes will be made to the disk /dev/sda :'),
+                  Container(height: 16),
+                  Text(
+                      'Fast Format/Erase\nCreate partitions "boot","recovery","conf","stateless","stateful"\nInstall bootloader\nInstall dahliaOS ' +
+                          systemVersion),
+                  Container(
+                    height: 16,
+                  ),
+                  Text("Is this OK?"),
+                  Container(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: this.install,
+                        onChanged: (bool value) {
+                          setState(() {
+                            this.install = value;
+                          });
+                        },
+                      ),
+                      Text("These changes are correct"),
+                    ],
+                  ),
+                ],
+              ),
+            )),
+          ),
+          Align(
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, left: 20, bottom: 15),
+                  child: OutlinedButton.icon(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back,
+                        size: 16,
+                      ),
+                      label: Text("Back")),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                        padding:
+                            EdgeInsets.only(top: 20.0, right: 20, bottom: 15),
+                        child: ElevatedButton(
+                            onPressed: install
+                                ? () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => AccountSetup()),
+                                    );
+                                  }
+                                : null,
                             child: Text('Install'))),
                   ],
                 )
